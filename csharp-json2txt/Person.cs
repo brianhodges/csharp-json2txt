d20178f9ac2adb3d0d441b5f4b7a467692376670
Development <Development@Brian-N-Hodgess-MacBook.local>
2017-05-06 20:04:00 -0400

    initial code commit

---
+++ csharp-json2txt/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace csharp_json2txt
+{
+    public class Person
+    {
+		public int id { get; set; }
+		public string first_name { get; set; }
+		public string last_name { get; set; }
+		public string email { get; set; }
+		public string btc_address { get; set; }
+		public string ip_address { get; set; }
+    }
+}
---
+++ csharp-json2txt/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace csharp_json2txt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "data.json";
+            using (var file = File.OpenRead(filename))
+            {
+                using (StreamReader r = new StreamReader(file))
+                {
+                    string json = r.ReadToEnd();
+                    List<Person> people = JsonConvert.DeserializeObject<List<Person>>(json);
+                    using (var f = File.CreateText("../log.txt"))
+                    {
+                        for (int x = 0; x < people.Count; x++)
+                        {
+                            f.WriteLine("ID: {0}", people[x].id);
+                            f.WriteLine("Full Name: {0} {1}", people[x].first_name, people[x].last_name);
+                            f.WriteLine("Email: {0}", people[x].email);
+                            f.WriteLine("BTC Address: {0}", people[x].btc_address);
+                            f.WriteLine("IP Address: {0}", people[x].ip_address);
+                            f.WriteLine();
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Done.");
+        }
+    }
+}
+
+
